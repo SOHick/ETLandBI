db9a5e66d1d58a7f24a6303e78a39bb701af5ba7
SOHick <shnikitaprog@mail.ru>
2024-05-22 20:18:36 +0300

    +sqlalchemy

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sqlalchemy import create_engine
+from psycopg2 import OperationalError
 import pandas as pd
 
 adult = pd.read_csv("data/netflix_titles.csv", encoding='latin1')
 adult.drop(adult.columns[12:], axis=1, inplace=True)
-print(adult.columns)
+
+def create_connection(db_name, db_user, db_password, db_host, db_port):
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            database=db_name,
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port,
+        )
+        print("Connection to PostgreSQL DB successful")
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+    return connection
