b0e20293111719cec305cae7b60914c466dda03f
SOHick <shnikitaprog@mail.ru>
2024-05-22 19:53:13 +0300

    +sqlalchemy

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
 import pandas as pd
+
+adult = pd.read_csv("data/netflix_titles.csv", encoding='latin1')
+adult.drop(adult.columns[12:], axis=1, inplace=True)
+print(adult.columns)
